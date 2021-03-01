0d07b0eda6c80ea0222e3852018378dd4e22ef67
b6320500638 <worakit.i@ku.th>
2021-03-01 19:46:37 +0700

    1,3

--- lab4_1.c
+++ lab4_1.c
@@ -1,1 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int n,max=0,min=1000,p,p1;
+    scanf("%d",&n);
+    int num[n],num1[3],max1[n-2];
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            scanf("%d",&num1[j]);
+        }
+        num[i]=(num1[0]*4)+(num1[1]*2)+(num1[2]*1);
+    }
+    for(int i=0;i<n-2;i++)
+    {
+          max1[i]=num[i]+num[i+1]+num[i+2];
+        if(max<max1[i])
+        {
+            max=max1[i];
+            p=i+1;
+        }
+        if(min>max1[i])
+        {
+            min=max1[i];
+            p1=i+1;
+        }
 
+    }
+printf("%d %d",p,p1);
+}
--- lab4_3.c
+++ lab4_3.c
@@ -1,1 +1,16 @@
-
+#include<stdio.h>
+int main()
+{
+    int j=0,i;
+    char word[200],check[200];
+    scanf("%s",word);
+        for(i=0;i<200;i++)
+        {
+            if(word[i]!=word[i+1])
+            {
+                check[j]=word[i];
+                j++;
+            }
+        }
+    printf("%s",check);
+}
